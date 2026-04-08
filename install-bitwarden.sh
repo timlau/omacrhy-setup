06d5f57f4e8bd0364ef4acedf0df3355c398f6bf
Tim Lauridsen <tla@rasmil.dk>
2026-04-08 16:37:53 +0200

    + setup bitwarden ssh agent for fish shell

--- install-bitwarden.sh
+++ install-bitwarden.sh
@@ -2,6 +2,15 @@
 
 omarchy-pkg-add bitwarden
 
+is_fish_installed() {
+  pacman -Qi "fish" &>/dev/null
+}
+
+if is_fish_installed; then
+  echo "setup for fish shell"
+  fish -c "set -gx SSH_AUTH_SOCK $HOME/.bitwarden-ssh-agent.sock"
+fi
+
 if grep -Fq "export SSH_AUTH_SOCK=" "$HOME/.bashrc"; then
   echo ".bashrc is already configured for bitwarden as ssh agent"
 else
@@ -9,4 +18,5 @@ else
   echo "# use bitwarden as ssh agent" >>$HOME/.bashrc
   echo "export SSH_AUTH_SOCK=$HOME/.bitwarden-ssh-agent.sock" >>$HOME/.bashrc
   echo "" >>$HOME/.bashrc
+
 fi
