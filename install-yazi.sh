ac09b1a40be1237c9062f3321d79c7756e962bcb
Tim Lauridsen <tla@rasmil.dk>
2026-04-13 16:16:01 +0200

    + yazi: install plugins amd improve output

--- install-yazi.sh
+++ install-yazi.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 theme_git="BennyOe/tokyo-night"
 theme=$(basename "$theme_git")
+plugins=(
+    "smart-enter"
+)
 
+# install yazi
 omarchy-pkg-add yazi
 
+# Install theme
 if [[ ! -f ~/.config/yazi/flavors/$theme.yazi/flavor.toml ]]; then
-  # install theme
-  echo " - -> installing $theme yazi theme "
-  ya pkg add $theme_git
+  echo "    -> installing $theme yazi theme "
+  ya pkg add $theme_git &>/dev/null
 fi
-# update plugins/flavours
-ya pkg upgrade
+
+# Install plugins
+for plugin in "${plugins[@]}"; do
+  if [[ ! -d ~/.config/yazi/plugins/$plugin.yazi ]]; then
+    # install plugin
+    echo "    -> installing $plugin yazi plugin "
+    ya pkg add yazi-rs/plugins:$plugin &>/dev/null
+  fi
+done
+
+# Update plugins/flavours
+ya pkg upgrade &>/dev/null
