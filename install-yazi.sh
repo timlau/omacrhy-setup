56e6ada2ae789cde5f9b5828afacbbbb6fbfaa42
Tim Lauridsen <tla@rasmil.dk>
2026-04-13 17:18:40 +0200

    + yazi: just use va pkg install to install every thing from package.toml

--- install-yazi.sh
+++ install-yazi.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
-theme_git="BennyOe/tokyo-night"
-theme=$(basename "$theme_git")
-plugins=(
-    "smart-enter"
-)
 
 # install yazi
 omarchy-pkg-add yazi
 
-# Install theme
-if [[ ! -d ~/.config/yazi/flavors/$theme.yazi ]]; then
-  echo "    -> installing $theme yazi theme "
-  ya pkg add $theme_git &>/dev/null
-fi
-
-# Install plugins
-for plugin in "${plugins[@]}"; do
-  if [[ ! -d ~/.config/yazi/plugins/$plugin.yazi ]]; then
-    # install plugin
-    echo "    -> installing $plugin yazi plugin "
-    ya pkg add yazi-rs/plugins:$plugin &>/dev/null
-  fi
-done
-
-# Update plugins/flavours
+# install or Update plugins/flavours
+ya pkg install &>/dev/null
 ya pkg upgrade &>/dev/null
