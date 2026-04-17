0601691539bbe7e9c7ce77ada8ff3c0ba493f3af
Tim Lauridsen <tla@rasmil.dk>
2026-04-17 19:20:41 +0200

    + update bitwarded setup

--- scripts/00-bitwarden.sh
+++ scripts/00-bitwarden.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-omarchy-pkg-add bitwarden
+env_file=$HOME/.config/uwsm/env
+packages=("bitwarden" "bitwarden-cli")
+use_bitwarden_as_ssh_agent=false
 
-is_fish_installed() {
-  pacman -Qi "fish" &>/dev/null
-}
-
-if is_fish_installed; then
-  echo "    -> Setup for fish shell"
-  fish -c "set -Ux SSH_AUTH_SOCK $HOME/.bitwarden-ssh-agent.sock"
+echo ${packages[@]}
+if omarchy-pkg-missing "${packages[@]}"; then
+  echo "Installing Bitwarden..."
+  omarchy-pkg-add "${packages[@]}"
+else
+  echo "Bitwarden already installed"
 fi
 
-if grep -Fq "export SSH_AUTH_SOCK=" "$HOME/.bashrc"; then
-  echo "    -> .bashrc is already configured for bitwarden as ssh agent"
-else
-  echo "    -> Configure bitwarden as ssh agent in bash"
-  echo "" >>$HOME/.bashrc
-  echo "# use bitwarden as ssh agent" >>$HOME/.bashrc
-  echo "export SSH_AUTH_SOCK=$HOME/.bitwarden-ssh-agent.sock" >>$HOME/.bashrc
-  echo "" >>$HOME/.bashrc
+if ! grep -Fq "SSH_AUTH_SOCK" $env_file; then
+  # ask user if they want to use Bitwarden as their SSH agent and configure SSH_AUTH_SOCK accordingly
+  if gum confirm "Use Bitwarden as SSH agent?"; then
+    use_bitwarden_as_ssh_agent=true
+    echo "export SSH_AUTH_SOCK=/home/$USER/.bitwarden-ssh-agent.sock" | tee -a $env_file &>/dev/null
+    echo "Make sure to enable SSH agent support in Bitwarden settings."
+  fi
+fi
 
+setsid gtk-launch bitwarden &>/dev/null
+
+if [[ $use_bitwarden_as_ssh_agent == "true" ]]; then
+  touch ~/.local/state/omarchy/reboot-required
+  omarchy-update-restart
 fi
