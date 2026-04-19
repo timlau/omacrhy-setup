1cc4295e5a398383e5a4727ccb6bbca5d956b491
Tim Lauridsen <tla@rasmil.dk>
2026-04-19 08:48:21 +0200

    bitwarden: only open bitwarden if not setup

--- scripts/00-bitwarden.sh
+++ scripts/00-bitwarden.sh
@@ -5,24 +5,23 @@ packages=("bitwarden" "bitwarden-cli")
 use_bitwarden_as_ssh_agent=false
 
 if omarchy-pkg-missing "${packages[@]}"; then
-  echo "Installing Bitwarden..."
-  omarchy-pkg-add "${packages[@]}"
+    echo "Installing Bitwarden..."
+    omarchy-pkg-add "${packages[@]}"
 else
-  echo "Bitwarden already installed"
+    echo "Bitwarden already installed"
 fi
 
 if ! grep -Fq "SSH_AUTH_SOCK" $env_file; then
-  # ask user if they want to use Bitwarden as their SSH agent and configure SSH_AUTH_SOCK accordingly
-  if gum confirm "Use Bitwarden as SSH agent?"; then
-    use_bitwarden_as_ssh_agent=true
-    echo "export SSH_AUTH_SOCK=/home/$USER/.bitwarden-ssh-agent.sock" | tee -a $env_file &>/dev/null
-    echo "Make sure to enable SSH agent support in Bitwarden settings."
-  fi
+    # ask user if they want to use Bitwarden as their SSH agent and configure SSH_AUTH_SOCK accordingly
+    if gum confirm "Use Bitwarden as SSH agent?"; then
+        use_bitwarden_as_ssh_agent=true
+        echo "export SSH_AUTH_SOCK=/home/$USER/.bitwarden-ssh-agent.sock" | tee -a $env_file &>/dev/null
+        echo "Make sure to enable SSH agent support in Bitwarden settings."
+        setsid gtk-launch bitwarden &>/dev/null
+    fi
 fi
 
-setsid gtk-launch bitwarden &>/dev/null
-
 if [[ $use_bitwarden_as_ssh_agent == "true" ]]; then
-  touch ~/.local/state/omarchy/reboot-required
-  omarchy-update-restart
+    touch ~/.local/state/omarchy/reboot-required
+    omarchy-update-restart
 fi
